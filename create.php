2a9f76a665395c1ab00d1e95c742d8c14b1567cd
orianetioni <79125296+orianetioni@users.noreply.github.com>
2021-06-09 10:52:34 +1100

    update

--- create.php
+++ create.php
@@ -1,7 +1,7 @@
 <?php
 include 'functions_custom.php';
-$pdo_Conn = pdo_connect_mysql ();
-
+$conn = pdo_connect_mysql ();
+$msg='';
 
 ?>
 
@@ -10,7 +10,42 @@ $pdo_Conn = pdo_connect_mysql ();
 </div>
 
 <?php 
-/**
- * ajouter le php necessaire
- */
+if (!empty($_POST)) {
+   
+    $id = isset($_POST['id']) && !empty($_POST['id']) && $_POST['id'] != 'auto' ? $_POST['id'] : NULL;
+	$first_name = isset($_POST['first_name']) ? $_POST['first_name'] : '';
+	$last_name = isset($_POST['last_name']) ? $_POST['last_name'] : '';
+	$email = isset($_POST['email']) ? $_POST['email'] : '';
+    $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+    $age= isset($_POST['age']) ? $_POST['age'] : '';
+    
+    $pdo_statement = $conn->prepare('INSERT INTO students VALUES (?, ?, ?, ?, ?, ?)');
+    $pdo_statement->execute([$id, $first_name, $last_name,$email, $phone]);
+    // Output message
+    $msg = 'Created Successfully!';
+}
 ?>
+
+<?=template_header('Create')?>
+
+<div class="content update">
+	<h2>Create Contact</h2>
+    <form action="create.php" method="post">
+        <label for="id">ID</label>
+		<input type="text" name="id" placeholder="26" value="auto" id="id">
+        <label for="name">Firstname</label>
+    	<input type="text" name="name"  id="name">
+		<label for="name">LastName</label>
+    	<input type="text" name="name"  id="name">
+        <label for="email">Email</label>
+        <label for="phone">Phone</label>
+        <input type="text" name="email" id="email">
+        <input type="text" name="phone" id="phone">
+       <input type="submit" value="Create">
+    </form>
+    <?php if ($msg): ?>
+    <p><?=$msg?></p>
+    <?php endif; ?>
+</div>
+
+<?=template_footer()?>
--- update.php
+++ update.php
@@ -1,15 +1,63 @@
 <?php 
-/**
- * ajouter le php necessaire
- */
+include 'functions_custom.php';
+$conn = pdo_connect_mysql();
+$msg = '';
+
+if (isset($_GET['id'])) {
+    if (!empty($_POST)) {
+        
+        $id = isset($_POST['id']) ? $_POST['id'] : NULL;
+        $first_name = isset($_POST['first_name']) ? $_POST['first_name'] : '';
+		$last_name = isset($_POST['last_name']) ? $_POST['last_name'] : '';
+		$email = isset($_POST['email']) ? $_POST['email'] : '';
+		$phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+		$age= isset($_POST['age']) ? $_POST['age'] : '';
+        // Update the record
+        $pdo_statement = $conn->prepare('UPDATE students SET id = ?, last_name = ?, first_name = ?,email = ?, phone = ?,age = ?, WHERE id = ?');
+        $pdo_statement->execute([$id, $first_name , $last_name, $email, $phone, $age, $_GET['id']]);
+        $msg = 'Updated Successfully!';
+    }
+    
+	$pdo_statement= $conn->prepare('SELECT * FROM students WHERE id = ?');
+	$pdo_statement->execute([$_GET['id']]);
+    $student = $pdo_statement->fetch(PDO::FETCH_ASSOC);
+    if (!$student) {
+        exit("Students doesn't exist");
+    }
+} else {
+    exit('No ID specified!');
+}
 ?>
+<?=template_header('Read')?>
 
 <div class="content update">
 	<h2>Update Student Guillaume</h2>
-</div>
+	<form action="update.php?id=<?=$student['id']?>" method="post">
+        <label for="id">ID</label>
+        <input type="text" name="id"  value="<?=$student['id']?>" id="id">
+
+        <label for="name">LastName</label>
+        <input type="text" name="last_name"  value="<?=$student['last_name']?>" id="id">
+      
+        <label for="name">FirstName</label>
+        <input type="text" name="first_name"  value="<?=$student['first_name']?>" id="id">
+       
+        <label for="email">Email</label>
+        <input type="text" name="email"  value="<?=$student['email']?>" id="email">
+        <label for="phone">Phone</label>
+     
+        <input type="text" name="phone"  value="<?=$student['phone']?>" id="phone">
+        <label for="title">Age</label>
+        <input type="text" name="title" placeholder="Employee" value="<?=$student['age']?>" id="title">
+        
+        <input type="submit" value="Update">
+    </form>
+
 
 <?php 
-/**
- * ajouter le php necessaire
- */
-?>
+ if ($msg): ?>
+    <p><?=$msg?></p>
+    <?php endif; ?>
+
+</div>
+<?=template_footer()?>
